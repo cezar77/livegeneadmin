448fd5a37d8d4b5d90784f9db6e67193cde3129c
Cezar Pendarovski <cezarpendarovski@web.de>
2018-10-03 06:00:14 +0100

    Add SDGRoleAdmin

---
+++ src/Admin/SDGRoleAdmin.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class SDGRoleAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('project')
+            ->add('sdg')
+            ->add('percent')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('percent');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id')
+            ->add('project')
+            ->add('sdg')
+            ->add('percent')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                )
+	        ))
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('project')
+            ->add('sdg')
+            ->add('percent')
+        ;
+    }
+}
