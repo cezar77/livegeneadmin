501f317506c43206dbe78896086a54a19511e268
Cezar Pendarovski <cezarpendarovski@web.de>
2018-10-07 10:37:34 +0100

    Add query parameters to getProjects in ProjectController

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -8,18 +8,36 @@ use FOS\RestBundle\View\View;
 use App\Entity\Project;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityNotFoundException;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 
 class ProjectController extends FOSRestController
 {
     /**
      * Retrieves a collection of Project resource
      * @Rest\Get("/projects")
+     * @QueryParam(map=true, name="ilriCode", requirements="\w+", strict=true, nullable=true,
+     *     description="Retrieves all projects for the given ILRI codes")
+     * @QueryParam(name="projectsGroup", requirements="\w+", strict=true, nullable=true,
+     *     description="Retrieves all projects for the given project's group")
      */
-    public function getProjects(): View
+    public function getProjects(ParamFetcher $paramFetcher): View
     {
         $repository = $this->getDoctrine()->getRepository(Project::class);
 
-        $data = $repository->findAll();
+        $criteria = $paramFetcher->all();
+
+        foreach ($criteria as $key => $value) {
+            if (is_null($value)) {
+                unset($criteria[$key]);
+            }
+        }
+
+        if ($criteria) {
+            $data = $repository->findBy($criteria);
+        } else {
+            $data = $repository->findAll();
+        }
 
         return View::create($data, Response::HTTP_OK);
     }
