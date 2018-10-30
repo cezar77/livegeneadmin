5c7f927ade0f683afb6170c2911a0d7c86c2b164
Cezar Pendarovski <cezarpendarovski@web.de>
2018-10-30 09:40:14 +0000

    Extend SamplingDocumentType short_name to 30 chars

--- src/Entity/SamplingDocumentType.php
+++ src/Entity/SamplingDocumentType.php
@@ -19,7 +19,7 @@ class SamplingDocumentType
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @ORM\Column(type="string", length=30)
      */
     private $shortName;
 
