87070aefcdb3e7b5d953c0e79e87ea4ade1e4700
Cezar Pendarovski <cezarpendarovski@web.de>
2019-01-31 13:37:05 +0000

    Try to reset to last working commit

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -6,12 +6,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\Traits\PersonTrait;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
  */
 class Contact
 {
+    use PersonTrait;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -23,27 +26,6 @@ class Contact
      * @ORM\Column(type="string", length=20, nullable=true)
      */
     private $title;
-
-    /**
-     * @ORM\Column(type="string", length=30)
-     * @Assert\NotBlank()
-     */
-    private $firstName;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     * @Assert\NotBlank()
-     */
-    private $lastName;
-
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true)
-     * @Assert\Email(
-     *     message = "The email '{{ value }}' is not a valid email.",
-     *     checkMX = true
-     * )
-     */
-    private $email;
 
     /**
      * @ORM\Column(type="string", length=30, nullable=true)
@@ -81,42 +63,6 @@ class Contact
     public function setTitle(?string $title): self
     {
         $this->title = $title;
-
-        return $this;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): self
-    {
-        $this->email = $email;
 
         return $this;
     }
